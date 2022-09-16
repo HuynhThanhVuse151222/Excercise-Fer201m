b296e15e15ba8f27115a2302240bfc1981432aaf
Thanhvu <vuhtse151222@fpt.edu.vn>
2022-09-16 19:28:28 +0700

    Components Part 1

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import './App.css';
+import Footer from './components/Footer';
+import Navigation from './components/Navigation';
+import Player from './Player';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Navigation/> 
+     <Player/>
+   <Footer/>
+    </div>
+  );
+}
+
+export default App;
