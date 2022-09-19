69b81493797696122335524b3687c1d82443448a
Thanhvu <vuhtse151222@fpt.edu.vn>
2022-09-19 20:46:29 +0700

    Final Lab1

--- my-app/src/components/PlayersPresentation.js
+++ my-app/src/components/PlayersPresentation.js
@@ -8,7 +8,7 @@ export default function PlayersPresentation({players}) {
         <div className='column'>
         <div className='card'>
         <img src={player.img} alt='pic'/>
-          <h3>{player.name}</h3>
+          <h3 className='name'>{player.name}</h3>
           <p className='title'>{player.club}</p>
           <p><button>Detail</button></p>
         </div>
